f5e5b879eaeac1c4a5687ca809adc33379962a54
cesine <cesine@yahoo.com>
2017-12-03 15:49:00 -0500

    converting timestamps to date period into a function
    which can take in a start and end date

--- src/lib/timestampsToDatePeriods.js
+++ src/lib/timestampsToDatePeriods.js
@@ -1,172 +1,176 @@
 const timestamps = require('../data.json');
 
-const data = timestamps.map((timestamp) => {
-  const date = new Date(timestamp);
-  const year = date.getFullYear();
-  const firstDayOfTheYear = new Date(`${year}-01-01`);
-  const millisecondsInADay = 60 * 60 * 1000 * 24;
-  const dayOfTheYear = (date - firstDayOfTheYear) / millisecondsInADay;
-  return {
-    timestamp,
-    date,
-    year: date.getFullYear(),
-    month: date.getMonth() + 1,
-    week: Math.floor(dayOfTheYear / 7) - 1,
-    weekday: date.getDay(),
-    hour: date.getHours(),
-  };
-});
+function timestampsToDatePeriods(start, end) {
+  const data = timestamps.map((timestamp) => {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const firstDayOfTheYear = new Date(`${year}-01-01`);
+    const millisecondsInADay = 60 * 60 * 1000 * 24;
+    const dayOfTheYear = (date - firstDayOfTheYear) / millisecondsInADay;
+    return {
+      timestamp,
+      date,
+      year: date.getFullYear(),
+      month: date.getMonth() + 1,
+      week: Math.floor(dayOfTheYear / 7) - 1,
+      weekday: date.getDay(),
+      hour: date.getHours(),
+    };
+  });
 
-const startDate = data[0];
-const endDate = data[data.length - 1];
+  const startDate = data[0];
+  const endDate = data[data.length - 1];
 
-const deploysPerDay = [];
-const deploysPerDayHistogram = [];
-const deploysPerDayHistogram2017 = [];
-const deploysPerWeek = [];
-const daysWithoutDeploys = [];
-const deploysByHour = [];
-const deploysByHour2017 = [];
-const deploysByDayOfTheWeek = [];
-const deploysByDayOfTheWeek2017 = [];
+  const deploysPerDay = [];
+  const deploysPerDayHistogram = [];
+  const deploysPerDayHistogram2017 = [];
+  const deploysPerWeek = [];
+  const daysWithoutDeploys = [];
+  const deploysByHour = [];
+  const deploysByHour2017 = [];
+  const deploysByDayOfTheWeek = [];
+  const deploysByDayOfTheWeek2017 = [];
 
-const years = [2016, 2017];
-const weeks = [];
-let i = 0;
-for (i = 0; i < 52; i++) {
-  weeks[i] = i + 1;
-}
-const weekdays = [];
-for (i = 0; i < 7; i++) {
-  weekdays[i] = i;
-  deploysByDayOfTheWeek[i] = {
-    weekday: i,
-    count: 0,
-  };
-  deploysByDayOfTheWeek2017[i] = {
-    weekday: i,
-    count: 0,
-  };
-}
-for (i = 0; i < 24; i++) {
-  deploysByHour[i] = {
-    hour: i,
-    count: 0,
-  };
-  deploysByHour2017[i] = {
-    hour: i,
-    count: 0,
-  };
-}
-
-years.forEach((year) => {
-  weeks.forEach((week) => {
-    if (year === startDate.year && week < startDate.week) {
-      return;
-    }
-    if (year === endDate.year && week > endDate.week) {
-      return;
-    }
-
-    deploysPerWeek.push({
-      yearWeek: `${year}w${week}`,
-      year,
-      week,
+  const years = [2016, 2017];
+  const weeks = [];
+  let i = 0;
+  for (i = 0; i < 52; i++) {
+    weeks[i] = i + 1;
+  }
+  const weekdays = [];
+  for (i = 0; i < 7; i++) {
+    weekdays[i] = i;
+    deploysByDayOfTheWeek[i] = {
+      weekday: i,
       count: 0,
-      deploys: [],
-    });
+    };
+    deploysByDayOfTheWeek2017[i] = {
+      weekday: i,
+      count: 0,
+    };
+  }
+  for (i = 0; i < 24; i++) {
+    deploysByHour[i] = {
+      hour: i,
+      count: 0,
+    };
+    deploysByHour2017[i] = {
+      hour: i,
+      count: 0,
+    };
+  }
 
-    weekdays.forEach((weekday) => {
-      deploysPerDay.push({
-        yearWeekDay: `${year}w${week}d${weekday}`,
+  years.forEach((year) => {
+    weeks.forEach((week) => {
+      if (year === startDate.year && week < startDate.week) {
+        return;
+      }
+      if (year === endDate.year && week > endDate.week) {
+        return;
+      }
+
+      deploysPerWeek.push({
+        yearWeek: `${year}w${week}`,
         year,
         week,
-        weekday,
         count: 0,
         deploys: [],
       });
+
+      weekdays.forEach((weekday) => {
+        deploysPerDay.push({
+          yearWeekDay: `${year}w${week}d${weekday}`,
+          year,
+          week,
+          weekday,
+          count: 0,
+          deploys: [],
+        });
+      });
     });
   });
-});
 
-let maxDeployCount = 0;
-data.forEach((datum) => {
-  deploysPerWeek.forEach((week) => {
-    if (week.year === datum.year && week.week === datum.week) {
-      // eslint-disable-next-line no-param-reassign
-      week.count++;
-      week.deploys.push(datum);
-    }
-  });
-  deploysPerDay.forEach((day) => {
-    if (day.year === datum.year && day.week === datum.week && day.weekday === datum.weekday) {
-      // eslint-disable-next-line no-param-reassign
-      day.count++;
-      day.deploys.push(datum);
-      if (day.count > maxDeployCount) {
-        maxDeployCount = day.count;
+  let maxDeployCount = 0;
+  data.forEach((datum) => {
+    deploysPerWeek.forEach((week) => {
+      if (week.year === datum.year && week.week === datum.week) {
+        // eslint-disable-next-line no-param-reassign
+        week.count++;
+        week.deploys.push(datum);
       }
+    });
+    deploysPerDay.forEach((day) => {
+      if (day.year === datum.year && day.week === datum.week && day.weekday === datum.weekday) {
+        // eslint-disable-next-line no-param-reassign
+        day.count++;
+        day.deploys.push(datum);
+        if (day.count > maxDeployCount) {
+          maxDeployCount = day.count;
+        }
+      }
+    });
+
+    deploysByHour[datum.hour].count++;
+    deploysByDayOfTheWeek[datum.weekday].count++;
+  });
+
+  // Prepare histogram
+  for (i = 0; i < maxDeployCount; i++) {
+    deploysPerDayHistogram[i] = {
+      freq: 0,
+      count: i,
+    };
+    deploysPerDayHistogram2017[i] = {
+      freq: 0,
+      count: i,
+    };
+  }
+
+  deploysPerDay.forEach((day) => {
+    // Only analyze working days
+    if (day.weekday !== 0 && day.weekday !== 6) {
+      if (!day.count) {
+        daysWithoutDeploys.push(day);
+      }
+      deploysPerDayHistogram[day.count] = deploysPerDayHistogram[day.count] || {
+        freq: 0,
+        count: day.count,
+      };
+      deploysPerDayHistogram[day.count].freq++;
     }
   });
 
-  deploysByHour[datum.hour].count++;
-  deploysByDayOfTheWeek[datum.weekday].count++;
-});
-
-// Prepare histogram
-for (i = 0; i < maxDeployCount; i++) {
-  deploysPerDayHistogram[i] = {
-    freq: 0,
-    count: i,
+  const only2017 = {
+    deploysPerDay: deploysPerDay.filter(day => day.year === 2017),
+    deploysPerWeek: deploysPerWeek.filter(day => day.year === 2017),
   };
-  deploysPerDayHistogram2017[i] = {
-    freq: 0,
-    count: i,
+
+  only2017.deploysPerDay.forEach((datum) => {
+    // Only analyze working days
+    if (datum.weekday !== 0 && datum.weekday !== 6) {
+      deploysPerDayHistogram2017[datum.count] = deploysPerDayHistogram2017[datum.count] || {
+        count: datum.count,
+        freq: 0,
+      };
+      deploysPerDayHistogram2017[datum.count].freq++;
+    }
+  });
+
+  return {
+    startDate,
+    endDate,
+    data,
+    deploysPerDay,
+    deploysPerDayHistogram,
+    deploysPerWeek,
+    daysWithoutDeploys,
+    deploysByHour,
+    deploysByDayOfTheWeek,
+
+    deploysPerDay2017: only2017.deploysPerDay,
+    deploysPerWeek2017: only2017.deploysPerWeek,
+    deploysPerDayHistogram2017,
   };
 }
 
-deploysPerDay.forEach((day) => {
-  // Only analyze working days
-  if (day.weekday !== 0 && day.weekday !== 6) {
-    if (!day.count) {
-      daysWithoutDeploys.push(day);
-    }
-    deploysPerDayHistogram[day.count] = deploysPerDayHistogram[day.count] || {
-      freq: 0,
-      count: day.count,
-    };
-    deploysPerDayHistogram[day.count].freq++;
-  }
-});
-
-const only2017 = {
-  deploysPerDay: deploysPerDay.filter(day => day.year === 2017),
-  deploysPerWeek: deploysPerWeek.filter(day => day.year === 2017),
-};
-
-only2017.deploysPerDay.forEach((datum) => {
-  // Only analyze working days
-  if (datum.weekday !== 0 && datum.weekday !== 6) {
-    deploysPerDayHistogram2017[datum.count] = deploysPerDayHistogram2017[datum.count] || {
-      count: datum.count,
-      freq: 0,
-    };
-    deploysPerDayHistogram2017[datum.count].freq++;
-  }
-});
-
-module.exports = {
-  startDate,
-  endDate,
-  data,
-  deploysPerDay,
-  deploysPerDayHistogram,
-  deploysPerWeek,
-  daysWithoutDeploys,
-  deploysByHour,
-  deploysByDayOfTheWeek,
-
-  deploysPerDay2017: only2017.deploysPerDay,
-  deploysPerWeek2017: only2017.deploysPerWeek,
-  deploysPerDayHistogram2017,
-};
+module.exports = timestampsToDatePeriods;
--- test/src/lib/timestampsToDatePeriods.js
+++ test/src/lib/timestampsToDatePeriods.js
@@ -1,144 +1,148 @@
-const index = require('../../../src/lib/timestampsToDatePeriods');
+const timestampsToDatePeriods = require('../../../src/lib/timestampsToDatePeriods');
 const expect = require('chai').expect;
 const fixtures = require('fixturefiles');
 
-describe('index', () => {
-  describe('current dates', () => {
-    it('should start in feb 2016', () => {
-      // console.log(index.startDate);
-      expect(index.startDate).to.deep.equal({
-        timestamp: '2016-02-17T23:01:32.000Z',
-        date: new Date('2016-02-17T23:01:32.000Z'),
-        year: 2016,
-        month: 2,
-        week: 5,
-        weekday: 3,
-        hour: 18,
-      });
-    });
+describe('timestampsToDatePeriods', () => {
+  describe('all data', () => {
+    const index = timestampsToDatePeriods();
 
-    it('should end in sept 2017', () => {
-      // console.log(index.endDate);
-      expect(index.endDate).to.deep.equal({
-        timestamp: '2017-10-03T19:15:38.000Z',
-        date: new Date('2017-10-03T19:15:38.000Z'),
-        year: 2017,
-        month: 10,
-        week: 38,
-        weekday: 2,
-        hour: 15,
-      });
-    });
-  });
-
-  describe('locale', () => {
-    it('should start week on sunday', () => {
-      const date = new Date('2017-09-10 10:00:00');
-      expect(date.getDay()).to.equal(0);
-      expect(date.toString()).to.equal('Sun Sep 10 2017 10:00:00 GMT-0400 (EDT)');
-    });
-
-    it('should end week on saturday', () => {
-      const date = new Date('2017-09-09 10:00:00');
-      expect(date.getDay()).to.equal(6);
-      expect(date.toString()).to.equal('Sat Sep 09 2017 10:00:00 GMT-0400 (EDT)');
-    });
-
-    it('should handle sql dates', () => {
-      const date = new Date('2016-02-17 18:01:32');
-      expect(date.getDay()).to.equal(3);
-      expect(date.toString()).to.equal('Wed Feb 17 2016 18:01:32 GMT-0500 (EST)');
-    });
-  });
-
-  describe('dates', () => {
-    it('should match fixture data', () => {
-      // console.log(JSON.stringify(index.data));
-      const strigifiedData = index.data.map((item) => {
-        if (item.date.toISOString) {
-          // eslint-disable-next-line no-param-reassign
-          item.date = item.date.toISOString();
-        }
-        return item;
-      });
-      expect(strigifiedData).to.deep.equal(fixtures.data);
-    });
-  });
-
-  describe('deploysPerDay', () => {
-    it('should match fixture deploysPerDay', () => {
-      // console.log(JSON.stringify(index.deploysPerDay));
-      const strigifiedData = index.deploysPerDay.map((item) => {
-        item.deploys.map((deploy) => {
-          if (deploy.date.toISOString) {
-            // eslint-disable-next-line no-param-reassign
-            deploy.date = deploy.date.toISOString();
-          }
-          return deploy;
+    describe('current dates', () => {
+      it('should start in feb 2016', () => {
+        // console.log(index.startDate);
+        expect(index.startDate).to.deep.equal({
+          timestamp: '2016-02-17T23:01:32.000Z',
+          date: new Date('2016-02-17T23:01:32.000Z'),
+          year: 2016,
+          month: 2,
+          week: 5,
+          weekday: 3,
+          hour: 18,
         });
-        return item;
       });
-      expect(strigifiedData).to.deep.equal(fixtures.deploysPerDay);
-    });
-  });
 
-  describe('deploysPerDayHistogram', () => {
-    it('should match fixture deploysPerDayHistogram', () => {
-      // console.log(JSON.stringify(index.deploysPerDayHistogram));
-      expect(index.deploysPerDayHistogram).to.deep.equal(fixtures.deploysPerDayHistogram);
-    });
-  });
-
-  describe('deploysPerWeek', () => {
-    it('should match fixture deploysPerWeek', () => {
-      // console.log(JSON.stringify(index.deploysPerWeek));
-      const strigifiedData = index.deploysPerWeek.map((item) => {
-        item.deploys.map((deploy) => {
-          if (deploy.date.toISOString) {
-            // eslint-disable-next-line no-param-reassign
-            deploy.date = deploy.date.toISOString();
-          }
-          return deploy;
+      it('should end in sept 2017', () => {
+        // console.log(index.endDate);
+        expect(index.endDate).to.deep.equal({
+          timestamp: '2017-10-03T19:15:38.000Z',
+          date: new Date('2017-10-03T19:15:38.000Z'),
+          year: 2017,
+          month: 10,
+          week: 38,
+          weekday: 2,
+          hour: 15,
         });
-        return item;
       });
-      expect(strigifiedData).to.deep.equal(fixtures.deploysPerWeek);
-    });
-  });
-
-  describe('daysWithoutDeploys', () => {
-    it('should match fixture daysWithoutDeploys', () => {
-      // console.log(JSON.stringify(index.daysWithoutDeploys));
-      expect(index.daysWithoutDeploys).to.deep.equal(fixtures.daysWithoutDeploys);
-    });
-  });
-
-  describe('deploysByHour', () => {
-    it('should match fixture deploysByHour', () => {
-      // console.log(JSON.stringify(index.deploysByHour));
-      expect(index.deploysByHour).to.deep.equal(fixtures.deploysByHour);
-    });
-  });
-
-  describe('deploysByDayOfTheWeek', () => {
-    it('should match fixture deploysByDayOfTheWeek', () => {
-      // console.log(JSON.stringify(index.deploysByDayOfTheWeek));
-      expect(index.deploysByDayOfTheWeek).to.deep.equal(fixtures.deploysByDayOfTheWeek);
-    });
-  });
-
-  describe('2017', () => {
-    it('should have expected number of  deploysPerDay', () => {
-      expect(index.deploysPerDay2017).length(266);
     });
 
-    it('should have expected number of deploysPerWeek', () => {
-      expect(index.deploysPerWeek2017).length(38);
+    describe('locale', () => {
+      it('should start week on sunday', () => {
+        const date = new Date('2017-09-10 10:00:00');
+        expect(date.getDay()).to.equal(0);
+        expect(date.toString()).to.equal('Sun Sep 10 2017 10:00:00 GMT-0400 (EDT)');
+      });
+
+      it('should end week on saturday', () => {
+        const date = new Date('2017-09-09 10:00:00');
+        expect(date.getDay()).to.equal(6);
+        expect(date.toString()).to.equal('Sat Sep 09 2017 10:00:00 GMT-0400 (EDT)');
+      });
+
+      it('should handle sql dates', () => {
+        const date = new Date('2016-02-17 18:01:32');
+        expect(date.getDay()).to.equal(3);
+        expect(date.toString()).to.equal('Wed Feb 17 2016 18:01:32 GMT-0500 (EST)');
+      });
     });
 
-    it('should match fixture deploysPerDayHistogram2017', () => {
-      // console.log(JSON.stringify(index.deploysPerDayHistogram2017));
-      expect(index.deploysPerDayHistogram2017).to.deep.equal(fixtures.deploysPerDayHistogram2017);
+    describe('dates', () => {
+      it('should match fixture data', () => {
+        // console.log(JSON.stringify(index.data));
+        const strigifiedData = index.data.map((item) => {
+          if (item.date.toISOString) {
+            // eslint-disable-next-line no-param-reassign
+            item.date = item.date.toISOString();
+          }
+          return item;
+        });
+        expect(strigifiedData).to.deep.equal(fixtures.data);
+      });
+    });
+
+    describe('deploysPerDay', () => {
+      it('should match fixture deploysPerDay', () => {
+        // console.log(JSON.stringify(index.deploysPerDay));
+        const strigifiedData = index.deploysPerDay.map((item) => {
+          item.deploys.map((deploy) => {
+            if (deploy.date.toISOString) {
+              // eslint-disable-next-line no-param-reassign
+              deploy.date = deploy.date.toISOString();
+            }
+            return deploy;
+          });
+          return item;
+        });
+        expect(strigifiedData).to.deep.equal(fixtures.deploysPerDay);
+      });
+    });
+
+    describe('deploysPerDayHistogram', () => {
+      it('should match fixture deploysPerDayHistogram', () => {
+        // console.log(JSON.stringify(index.deploysPerDayHistogram));
+        expect(index.deploysPerDayHistogram).to.deep.equal(fixtures.deploysPerDayHistogram);
+      });
+    });
+
+    describe('deploysPerWeek', () => {
+      it('should match fixture deploysPerWeek', () => {
+        // console.log(JSON.stringify(index.deploysPerWeek));
+        const strigifiedData = index.deploysPerWeek.map((item) => {
+          item.deploys.map((deploy) => {
+            if (deploy.date.toISOString) {
+              // eslint-disable-next-line no-param-reassign
+              deploy.date = deploy.date.toISOString();
+            }
+            return deploy;
+          });
+          return item;
+        });
+        expect(strigifiedData).to.deep.equal(fixtures.deploysPerWeek);
+      });
+    });
+
+    describe('daysWithoutDeploys', () => {
+      it('should match fixture daysWithoutDeploys', () => {
+        // console.log(JSON.stringify(index.daysWithoutDeploys));
+        expect(index.daysWithoutDeploys).to.deep.equal(fixtures.daysWithoutDeploys);
+      });
+    });
+
+    describe('deploysByHour', () => {
+      it('should match fixture deploysByHour', () => {
+        // console.log(JSON.stringify(index.deploysByHour));
+        expect(index.deploysByHour).to.deep.equal(fixtures.deploysByHour);
+      });
+    });
+
+    describe('deploysByDayOfTheWeek', () => {
+      it('should match fixture deploysByDayOfTheWeek', () => {
+        // console.log(JSON.stringify(index.deploysByDayOfTheWeek));
+        expect(index.deploysByDayOfTheWeek).to.deep.equal(fixtures.deploysByDayOfTheWeek);
+      });
+    });
+
+    describe('2017', () => {
+      it('should have expected number of  deploysPerDay', () => {
+        expect(index.deploysPerDay2017).length(266);
+      });
+
+      it('should have expected number of deploysPerWeek', () => {
+        expect(index.deploysPerWeek2017).length(38);
+      });
+
+      it('should match fixture deploysPerDayHistogram2017', () => {
+        // console.log(JSON.stringify(index.deploysPerDayHistogram2017));
+        expect(index.deploysPerDayHistogram2017).to.deep.equal(fixtures.deploysPerDayHistogram2017);
+      });
     });
   });
 });
